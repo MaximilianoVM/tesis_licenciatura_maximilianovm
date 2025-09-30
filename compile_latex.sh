1b4a0d76ff98ac9299f17bdee56a53898b037580
MaximilianoVM <maximiliano.vaca@uabc.edu.mx>
2025-09-30 15:29:07 -0700

    ✨ Diagrama en svg y mas texto en sustraccion

--- compile_latex.sh
+++ compile_latex.sh
@@ -1,21 +1,5 @@
-#!/bin/bash
-
-# Ejecutar pdflatex hasta que no haya más referencias pendientes
-max_iterations=5
-for i in $(seq 1 $max_iterations); do
-  pdflatex -interaction=nonstopmode Tesis
-done
-
-# Generar bibliografía
+pdflatex Tesis
 bibtex Tesis
-
-# Generar glosarios
 glosstex Tesis acronimos.gdf
-
-# Generar índices
 makeindex Tesis.gxs -o Tesis.glx -s glosstex.ist
-
-# Compilar el documento nuevamente para reflejar todos los cambios
-for i in $(seq 1 2); do
-  pdflatex -interaction=nonstopmode Tesis
-done
+pdflatex Tesis
